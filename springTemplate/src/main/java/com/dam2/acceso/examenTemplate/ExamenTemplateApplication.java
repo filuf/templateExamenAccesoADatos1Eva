e3935431c9d6cd8b41173a4a7d5c82b79c1d63ac
adminaitor <aitorpj93@gmail.com>
2025-11-25 18:13:02 +0100

    creacion del proyecto inicial con Spring initializr

---
+++ springTemplate/src/main/java/com/dam2/acceso/examenTemplate/ExamenTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.dam2.acceso.examenTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenTemplateApplication.class, args);
+	}
+
+}
---
+++ springTemplate/src/test/java/com/dam2/acceso/examenTemplate/ExamenTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam2.acceso.examenTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
